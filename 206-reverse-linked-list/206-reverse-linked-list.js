0b16b75276fe6435043deb155cf56268a1e19050
Shami Tomita <70528966+ShamiTomita@users.noreply.github.com>
2022-05-29 19:09:17 -0400

    Time: 83 ms (53.39%), Space: 44.4 MB (43.25%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    // End of the reversed linked list set to null
+    let prev = null;
+    
+    // Traverse through the given linked list
+    while (head) {
+        const temp = head.next;     // References the next Node of given linked list
+        head.next = prev;       // head.next point to previous Node, instead of the next Node of the given linked list
+        prev = head;        // Move the prev Node pointer up to head
+        head = temp;        // Move the head up to next Node of the given linked list
+    }
+    
+    // Prev is the reversed linked list
+    return prev;
+}
