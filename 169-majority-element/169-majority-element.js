2c332c9ff8f0474eb25151074e4f7c7961c98676
Shami Tomita <70528966+ShamiTomita@users.noreply.github.com>
2022-06-01 10:46:04 -0400

    Time: 135 ms (9.61%), Space: 44.4 MB (40.71%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,20 +3,7 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const fqCounter = {};
-    for (let val of nums){
-        fqCounter[val] = (fqCounter[val] || 0) + 1
-    }
-    let current = 0; 
-    let maxKey;
-    for(let key in fqCounter){
-        console.log(fqCounter)
-        console.log(fqCounter[key], current)
-        if(fqCounter[key] > current){
-            current = fqCounter[key];
-            maxKey = key
-        }
-        console.log(current)
-    }
-    return maxKey;
-};
+    // sort the array and the middle is the majority
+    nums.sort((a,b) => a - b);
+    return nums[Math.floor(nums.length/2)];
+}; 
